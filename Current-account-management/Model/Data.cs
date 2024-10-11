307dabfab9189b2583f59bdf2a93da9c97321baa
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-10-11 15:42:44 -0300

    create class Data

---
+++ Current-account-management/Model/Data.cs
@@ -0,0 +1,18 @@
+﻿namespace ContaCorrente.Model;
+
+
+public static class Data
+{
+    //8)Como garantir que datas como 31/2/2005 não sejam aceitas pela sua classe Data?
+    public static bool DataEhValida(string data)
+    {
+        DateTime dataConvertida;
+        if (DateTime.TryParse(data, out dataConvertida))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
