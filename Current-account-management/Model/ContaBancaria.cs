c5a86b9088e6c6da0bee3f5243bc699236040720
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-10-11 15:41:44 -0300

    create class ContaBancaria

---
+++ Current-account-management/Model/ContaBancaria.cs
@@ -0,0 +1,25 @@
+﻿namespace ContaCorrente.Model
+{
+    internal class ContaBancaria
+    {
+        private List<Movimentacao> movimentacoes = new();
+
+        public ContaBancaria(int numeroConta, Titular titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+
+        public int NumeroConta { get; }
+        public double Saldo { get; set;  }
+        public bool Especial { get; set; }
+        public Titular Titular { get; }
+        public Movimentacao Movimentacao { get; }
+
+
+        public virtual void RegistrarMovimentacao( Movimentacao movimentacao)
+        {
+            movimentacoes.Add(movimentacao);
+        }
+    }
+}
