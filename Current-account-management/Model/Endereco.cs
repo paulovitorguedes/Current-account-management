784a0952b7fea7661d32ce72e0d214f8e4f09c4a
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-10-11 15:40:47 -0300

    create class Endereço

---
+++ Current-account-management/Model/Endereco.cs
@@ -0,0 +1,16 @@
+﻿namespace ContaCorrente.Model;
+
+internal class Endereco
+{
+    public string Rua { get; set; } = string.Empty;
+    public string Bairro { get; set; } = string.Empty;
+    public string Cidade { get; set; } = string.Empty;
+
+
+    string GerarEndereço()
+    {
+        return $"Rua: {Rua} - Bairro: {Bairro} - {Cidade}";
+    }
+}
+
+
