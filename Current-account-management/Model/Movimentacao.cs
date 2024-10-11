953c127fb732c8b439c41181187341106cdf18a1
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-10-11 15:42:14 -0300

    create class Movimentacao

---
+++ Current-account-management/Model/Movimentacao.cs
@@ -0,0 +1,13 @@
+﻿namespace ContaCorrente.Model
+{
+    internal class Movimentacao
+    {
+        public double valor;
+        public TipoMovimentacao movimentacao;
+
+        public enum TipoMovimentacao
+        {
+            Credito, Debito, saque, transferencia
+        }
+    }
+}
